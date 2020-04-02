c881f314d2a6855e6904e794c37a5021a4d49cb7
Salma Shaik <salmashaik1611@gmail.com>
2020-04-02 11:06:18 -0400

    getting updated ol files

--- filters/outliers_remover.py
+++ filters/outliers_remover.py
@@ -20,5 +20,12 @@ fnl_mn_without_lower_3z = fnl_mn[~fnl_mn.ori_yr.isin(fnl_mn_ols.ori_yr)]
 
 print('fnl_mn_without_lower_3z: ', fnl_mn_without_lower_3z.shape[0])
 
-agncy_cat.categorize_agencies(df=fnl_mn_without_lower_3z, op_path='/Users/salma/Research/us_crime_data_analysis/data',
+## rounding population_mean to have count.
+# fnl_mn_without_lower_3z.loc[:, 'population_mean'] = fnl_mn_without_lower_3z['population_mean'].round()
+
+
+## In cleaned_icpsr_crime_enhancer.py, was using 100000 multiplier for incarceration too.
+## But 10000 in initial rates.py. So need to update incarceration rates to be multiplied with only 10000 and not 100000
+
+agncy_cat.categorize_agencies(df=fnl_mn_without_lower_3z, op_path='/Users/salma/Research/us_crime_data_analysis/data/wdt_lower_3z',
                               fl_name='final_main_without_lower_3z')
